01e000cef58e90074bfe88681baf91494ffeb95b
ramyabyyu <bintantramy@gmail.com>
2020-09-05 05:56:33 +0700

    Edit a litle bit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const crudApi = require("./route/api/crudApi");
+const path = require("path");
 
 const app = express();
 
@@ -24,6 +25,17 @@ mongoose
     Set up APIs
 */
 app.use("/api/item", crudApi);
+
+/* 
+    Server static assets if in production
+*/
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 /* 
     Listen to the Server
